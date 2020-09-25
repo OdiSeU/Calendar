bf5bf88b314300ceaf75bd6b064306cb6104661d
OdiSeU <secret36565@gmail.com>
2020-09-26 00:14:54 +0900

    200926 update
    create clock object

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+const Clock = (function() {
+  let date = new Date();
+  return {
+    year : 0,
+    month : 0,
+    date : 0,
+    day : 0,
+    hour : 0,
+    minute : 0,
+    second : 0,
+    update : function() {
+
+    },
+    toString : function() {
+
+    }
+  }
+})();
---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  let topBar = document.querySelector('#top-bar-container');
+  let leftBar = document.querySelector('#left-bar-container');
+  let rightBar = document.querySelector('#right-bar-container');
+
+  let resizeBars = document.getElementsByClassName('resize-bar');
+  let leftResizeBar = resizeBars[0];
+  let rightResizeBar = resizeBars[1];
+
+  leftResizeBar.onmousemove = function(e) {
+    if(e.buttons == 1) {
+      this.parentNode.style.width = (e.x + this.offsetWidth/2) + 'px';
+    }
+  }
+}

e9585df30a776beaf8827b786ddf6a509df5dcf8
OdiSeU <secret36565@gmail.com>
2020-09-26 17:27:02 +0900

    200926 update
    update clock object

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,10 @@
 const Clock = (function() {
-  let date = new Date();
-  return {
+  let clock = {
+    days : {
+      'en' : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+      'kr' : ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'],
+      'ch' : ['日','月','火','水','木','金','土']
+    },
     year : 0,
     month : 0,
     date : 0,
@@ -9,10 +13,15 @@ const Clock = (function() {
     minute : 0,
     second : 0,
     update : function() {
-
+      let date = new Date();
+      this.year = date.getYear();
+      this.month = date.getMonth() + 1;
+      this.date = date.getDate();
+      this.day =
     },
     toString : function() {
 
     }
   }
+  return
 })();
